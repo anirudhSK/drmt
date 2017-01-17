26a681245bb382fe66349d2840b0a716d87df95f
Anirudh Sivaraman <sk.anirudh@gmail.com>
2017-01-17 09:35:25 -0500

    Simplify benchmark.sh; commit results on Andy's precise DAG

--- benchmark.sh
+++ benchmark.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 
+if [ $# -ne 1 ]; then
+  echo "Usage: ./benchmark.sh result_folder"
+  exit
+fi
+
 for arch in drmt prmt_fine prmt_coarse; do
-  for prog in switch-orig-egress-ilp-input switch-orig-ingress-ilp-input; do
-    for greedy in yes no; do
-      echo "$arch $prog $greedy"
-      time python $arch.py $prog $greedy > ${arch}_${prog}_${greedy}.txt
-    done
+  for prog in switch_egress switch_ingress; do
+    echo "$arch $prog"
+    /usr/bin/time python $arch.py $prog yes > $1/${arch}_${prog}.txt
+    echo -e "\n"
   done
 done
