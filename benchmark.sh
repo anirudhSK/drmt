b430b44bebfde776f3c178bddabe2df95c2d6360
Anirudh Sivaraman <sk.anirudh@gmail.com>
2017-01-18 12:09:10 -0500

    Update precise DAG results

--- benchmark.sh
+++ benchmark.sh
@@ -5,10 +5,12 @@ if [ $# -ne 1 ]; then
   exit
 fi
 
-for arch in drmt prmt_fine prmt_coarse; do
-  for prog in switch_egress switch_ingress; do
-    echo "$arch $prog"
-    /usr/bin/time python $arch.py $prog yes > $1/${arch}_${prog}.txt
+for prog in switch_egress switch_ingress; do
+    echo "drmt $prog"
+    /usr/bin/time python drmt.py $prog yes > $1/drmt_${prog}.txt
+    echo "prmt_fine $prog"
+    /usr/bin/time python prmt.py $prog yes fine > $1/prmt_fine_${prog}.txt
+    echo "prmt_coarse $prog"
+    /usr/bin/time python prmt.py $prog yes coarse > $1/prmt_coarse_${prog}.txt
     echo -e "\n"
   done
-done
