2bc54ecc119fe740c588ee86dda0ea86c1425cb9
Anirudh Sivaraman <sk.anirudh@gmail.com>
2017-01-23 22:55:50 -0500

    Make this 30; switch_ingress has a 29 field action

--- small_hw.py
+++ small_hw.py
@@ -1,6 +1,6 @@
 # Match key and action field limit for each processor
 # We assume the processors don't share resources for now
-action_fields_limit = 20
+action_fields_limit = 30
 match_unit_limit    = 5
 match_unit_size     = 80
 action_proc_limit   = 1
