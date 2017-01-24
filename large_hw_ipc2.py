77e140e85b7c25a87c3f442d83cfe0b9c73ea166
Anirudh Sivaraman <sk.anirudh@gmail.com>
2017-01-23 23:29:32 -0500

    Remove misleading comment

--- large_hw_ipc2.py
+++ large_hw_ipc2.py
@@ -1,5 +1,3 @@
-# Match key and action field limit for each processor
-# We assume the processors don't share resources for now
 action_fields_limit = 32
 match_unit_limit = 8
 match_unit_size  = 80
