28e001009fb145b52041468f64352680a2c6c5ed
shayvar <vargaftik@gmail.com>
2017-06-04 23:40:06 +0300

    Update random_odg_generator.py

--- random_odg_generator.py
+++ random_odg_generator.py
@@ -22,9 +22,7 @@ def digraph_generator(n):
     # nx.draw(G)
     
     return G
-
-# TODO: Match should have a corresponding action - split match to match-action and decrease primitive action probability?
-
+    
 def odg_attr_generator(G, delays):
     
     nodes = {}
@@ -42,14 +40,15 @@ def odg_attr_generator(G, delays):
         
       if successors:
         
-          node_type = np.random.choice(['_condition_','_MATCH','_ACTION'], p=[0.24, 0.38, 0.38])
+          node_type = np.random.choice(['_condition_','MATCH_ACTION','P_ACTION'], p=[0.25, 0.6, 0.15])
           
       else:
           
-          node_type = np.random.choice(['_MATCH','_ACTION'], p=[0.5, 0.5])
+          node_type = np.random.choice(['MATCH_ACTION','P_ACTION'], p=[0.85, 0.15])
           
+      # match then action nodes.    
+      if node_type == 'MATCH_ACTION':
           
-      if node_type == '_MATCH':
           
         # Geometric
         key_width = 80*int(min(np.random.geometric(.75, 1),8))
@@ -57,9 +56,21 @@ def odg_attr_generator(G, delays):
         # uniform
         # key_width = 80*np.random.random_integers(1, 8)
                 
-        nodes[str(node)+node_type] = {'key_width': key_width, 'type': 'match', 'ID': node}
-                               
-      elif node_type == '_ACTION':
+        nodes[str(node)+'_MATCH'] = {'key_width': key_width, 'type': 'match', 'ID': node}
+        
+        # Geometric
+        num_fields = int(min(np.random.geometric(.25, 1),32))
+        
+        # uniform
+        # num_fields = np.random.random_integers(1, 32)
+        
+        nodes[str(node)+'_ACTION'] = {'num_fields': num_fields, 'type': 'action', 'ID': node}
+        
+        # Create corresponding edge
+        edges[(str(node)+'_MATCH', str(node)+'_ACTION')] = {'delay': dm, 'dep_type': 'new_match_to_action'}
+        
+      # Action with no corresponding match.                         
+      elif node_type == 'P_ACTION':
           
         # Geometric
         num_fields = int(min(np.random.geometric(.25, 1),32))
@@ -68,7 +79,8 @@ def odg_attr_generator(G, delays):
         # num_fields = np.random.random_integers(1, 32)
             
         nodes[str(node)+node_type] = {'num_fields': num_fields, 'type': 'action', 'ID': node}
-                
+      
+      # Condition.          
       else:
             
         nodes[node_type + str(node)] = {'num_fields': 1, 'type': 'condition', 'ID': node}
@@ -87,51 +99,52 @@ def odg_attr_generator(G, delays):
           # name according to ID                        
           n_dest = [name for name in nodes if nodes[name]['ID']==dest][0] 
 
+          # match-action edges allready exist.  
+          if  (n_node, n_dest) not in edges:            
+
+              # TODO: fill correct 'dep_type'.                         
+                                        
+              if nodes[n_node]['type'] == 'match' and nodes[n_dest]['type'] == 'match':
+                                
+                edges[(n_node, n_dest)] = {'delay': dm, 'dep_type': 'new_match_to_action'}
+    
+              if nodes[n_node]['type'] == 'match' and nodes[n_dest]['type'] == 'action':
+                                
+                edges[(n_node, n_dest)] = {'delay': dm, 'dep_type': 'new_match_to_action'}
+                
+              if nodes[n_node]['type'] == 'match' and nodes[n_dest]['type'] == 'condition':
+                                
+                edges[(n_node, n_dest)] = {'delay': dm, 'dep_type': 'new_match_to_action'}
+    
+                
+                
+              if nodes[n_node]['type'] == 'action' and nodes[n_dest]['type'] == 'match':
+                                
+                edges[(n_node, n_dest)] = {'delay': da, 'dep_type': 'rmt_match'}
+    
+              if nodes[n_node]['type'] == 'action' and nodes[n_dest]['type'] == 'action':
+                                
+                edges[(n_node, n_dest)] = {'delay': da, 'dep_type': 'rmt_match'}
+    
+              if nodes[n_node]['type'] == 'action' and nodes[n_dest]['type'] == 'condition':
+                                
+                edges[(n_node, n_dest)] = {'delay': da, 'dep_type': 'rmt_match'}
+                
+                
+    
+              if nodes[n_node]['type'] == 'condition' and nodes[n_dest]['type'] == 'match':
+                                
+                edges[(n_node, n_dest)] = {'delay': ds, 'dep_type': 'rmt_successor'}
+    
+              if nodes[n_node]['type'] == 'condition' and nodes[n_dest]['type'] == 'action':
+                                
+                edges[(n_node, n_dest)] = {'delay': ds, 'dep_type': 'rmt_successor'}
+    
+              if nodes[n_node]['type'] == 'condition' and nodes[n_dest]['type'] == 'condition':
+                                
+                edges[(n_node, n_dest)] = {'delay': ds, 'dep_type': 'rmt_successor'}
+                            
                        
-
-          # TODO: fill correct 'dep_type'.                         
-                                    
-          if nodes[n_node]['type'] == 'match' and nodes[n_dest]['type'] == 'match':
-                            
-            edges[(n_node, n_dest)] = {'delay': dm, 'dep_type': 'new_match_to_action'}
-
-          if nodes[n_node]['type'] == 'match' and nodes[n_dest]['type'] == 'action':
-                            
-            edges[(n_node, n_dest)] = {'delay': dm, 'dep_type': 'new_match_to_action'}
-            
-          if nodes[n_node]['type'] == 'match' and nodes[n_dest]['type'] == 'condition':
-                            
-            edges[(n_node, n_dest)] = {'delay': dm, 'dep_type': 'new_match_to_action'}
-
-            
-            
-          if nodes[n_node]['type'] == 'action' and nodes[n_dest]['type'] == 'match':
-                            
-            edges[(n_node, n_dest)] = {'delay': da, 'dep_type': 'rmt_match'}
-
-          if nodes[n_node]['type'] == 'action' and nodes[n_dest]['type'] == 'action':
-                            
-            edges[(n_node, n_dest)] = {'delay': da, 'dep_type': 'rmt_match'}
-
-          if nodes[n_node]['type'] == 'action' and nodes[n_dest]['type'] == 'condition':
-                            
-            edges[(n_node, n_dest)] = {'delay': da, 'dep_type': 'rmt_match'}
-            
-            
-
-          if nodes[n_node]['type'] == 'condition' and nodes[n_dest]['type'] == 'match':
-                            
-            edges[(n_node, n_dest)] = {'delay': ds, 'dep_type': 'rmt_successor'}
-
-          if nodes[n_node]['type'] == 'condition' and nodes[n_dest]['type'] == 'action':
-                            
-            edges[(n_node, n_dest)] = {'delay': ds, 'dep_type': 'rmt_successor'}
-
-          if nodes[n_node]['type'] == 'condition' and nodes[n_dest]['type'] == 'condition':
-                            
-            edges[(n_node, n_dest)] = {'delay': ds, 'dep_type': 'rmt_successor'}
-            
-                                       
     return nodes, edges
     
             
@@ -146,4 +159,5 @@ def odg_generator(n):
     # generate ODG
     return odg_attr_generator(G, delays)
   
-# print odg_generator(10)
+
+print odg_generator(10)
